f65e9bd7c4bcc8808367a394ee1cf3a756f97eca
AIAEL-GARHY <ayaelgarhy925@gmail.com>
2024-09-27 20:55:02 +0300

    TIMER_interface.h

--- 3-Mcal/TIMER_Driver/TIMER_interface.h
+++ 3-Mcal/TIMER_Driver/TIMER_interface.h
@@ -11,8 +11,10 @@
 #ifndef _TIMER_INTERFACE_H_
 #define _TIMER_INTERFACE_H_
 
-#define TIMER_OVF_INT 0
-#define TIMER_CTC_INT 1
+#define TIMER0_CTC_MODE            0
+#define TIMER0_OVF_MODE            1
+#define TIMER0_Fast_PWM_MODE       2
+#define TIMER0_Phase_Correct_MODE  3
 
 
 void TIMER0_voidInit(void);                                        /*Used to Initialize Timer_0*/
@@ -24,6 +26,9 @@ void TIMER1_voidInit(void);                                        /*Used to Ini
 void TIMER1_voidSetTimerValue(u16 copy_u16TimerValue);             /*Used to Set Value Timer_1*/
 
 u16 TIMER1_voidGetTimerValue(void);                                /*Used to Get Value of Timer_1*/
+
+void TIMER1_voidSetCompareValueChannelA(u16 copy_u16CompareValue)
+
 
 #endif
 
